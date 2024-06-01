aee0ec858d66ccc3a7059899a37c21b0c5f6de48
bfloyd14 <brendanflo1425@gmail.com>
2024-06-01 17:49:02 -0400

    add auth, login and sign up templates.

--- main_app/views.py
+++ main_app/views.py
@@ -10,3 +10,19 @@ from .models import Pizza
 
 class Home(LoginView):
   template_name = 'home.html'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('cat-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context) 
+    
