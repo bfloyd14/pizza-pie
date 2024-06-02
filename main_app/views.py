991d85fff96f1186b60b4b905fb374f962d9f47c
bfloyd14 <brendanflo1425@gmail.com>
2024-06-02 12:40:30 -0400

    add about url, template, view and add model to admin.

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Pizza
 
 # Create your views here.
 
 class Home(LoginView):
   template_name = 'home.html'
+
+def about(request):
+  return render(request, 'about.html')  
 
 def signup(request):
   error_message = ''
@@ -25,4 +30,6 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'signup.html', context) 
+
+
     
--- pizzapie/settings.py
+++ pizzapie/settings.py
@@ -9,12 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
-from pathlib import Path
-
 import environ
 import os
 env = environ.Env()
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,7 +127,7 @@ STATIC_URL = 'static/'
 
 LOGIN_URL = 'home'
 
-
+LOGOUT_REDIRECT_URL = 'home'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
